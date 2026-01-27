3606eb8b77873dd438bf9fd3339a0bf1e59ad0b0
Weston <classicmclive@gmail.com>
2026-01-26 21:30:53 -0500

    Add neufrere.js for interactive drawing application

---
+++ neufrere.js
@@ -0,0 +1,151 @@
+let cx = 1920;
+let cy = 1080;
+let debug = false;
+let toConjugate = false;
+let modifier = 125;
+
+function save(sed){german = sed;}
+let rects = [[595,529,310,116],[989,529,310,116],[595,689,310,116],[989,689,310,116]]
+createCanvas(cx,cy)
+bg("green")
+let index = 0;
+let question = [german[index].g,german[index].e,[german[index].a,german[index].b,german[index].c,german[index].d]];
+
+function shuffle(array) {let currentIndex = array.length;while (currentIndex != 0) {let randomIndex = Math.floor(Math.random() * currentIndex);currentIndex--;[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];}}
+shuffle(question[2]);
+
+let isCorrect = undefined;
+let counter = [0,0];
+
+/* Square Maker */
+mouseWillMove();
+let storedPos = [];
+let currentPos = []
+function startUp(){
+    // alert("uau")
+    storedPos = [mouse.x,mouse.y]
+}
+
+try{
+    if(con[0]==true){
+        toConjugate = true;
+    }
+}
+catch(error){
+    console.log(error)
+}
+
+mouseClicked(startUp)
+function drawDebugger(){
+    selectColor("white");
+    text(`MX: ${mouse.x}, MY: ${mouse.y}, isCorrect: ${isCorrect}, Count: ${counter[0]}/${counter[1]}, I:${index}`,cx/2,cy-30,"40px Serif")
+    text(`I/P: ${data.val}`,cx/2,cy-80,"40px Serif")
+    circle(mouse.x,mouse.y,20)
+    rect(storedPos[0],storedPos[1],mouse.x-storedPos[0],mouse.y-storedPos[1])
+    text(mouse.x-storedPos[0],storedPos[0]+(textWidth(mouse.x-storedPos[0],"20px Serif")/2),storedPos[1]-10,"20px Serif")
+    text(mouse.y-storedPos[1],storedPos[0]+(textWidth(mouse.x-storedPos[0],"20px Serif")/2),mouse.y+22,"20px Serif")
+}
+useKeys();
+function up(){
+    if(debug){storedPos=[];}
+}
+document.addEventListener('keydown', function(event) {
+    if (event.key === '`') {
+      debug = !debug;
+      document.getElementById("texto").blur();
+    }
+});
+function down(){if(debug){document.getElementById("texto").focus()}}
+// function left(){debug=!debug;}
+function right(){if(debug){document.getElementById("texto").blur()}}
+
+
+
+/* Real Drawing Pipeline */
+
+function rectangles(){
+    for(let i=0;i<rects.length;i++){
+        let r = rects[i];
+        selectColor("white");
+        if(isInside(mouse,{x:r[0],y:r[1],width:r[2],height:r[3]})&&isCorrect==undefined){selectColor("green");}
+        if(isCorrect&&question[2][i][1]){selectColor("lightgreen");}else if(isCorrect!=undefined&&!isCorrect&&!question[2][i][1]){selectColor("red")}
+        rect(r[0],r[1],r[2],r[3])
+        selectColor("black")
+        text(question[2][i][0],r[0]+(r[2]/2),r[1]+75,"60px Serif")
+        selectColor("white");    
+    }
+}
+
+function newQuestion(){
+    if(index>=german.length){
+        index = 0;
+    }
+    isCorrect = undefined;
+    question = [german[index].g,german[index].e,[german[index].a,german[index].b,german[index].c,german[index].d]];
+    shuffle(question[2]);
+}
+
+mouseClicked(()=>{
+    for(let i=0;i<rects.length;i++){
+        let r = rects[i];
+        if(isInside(mouse,{x:r[0],y:r[1],width:r[2],height:r[3]})){
+            if(isCorrect==undefined){
+                counter[1]++;
+                index++;
+                if(question[2][i][1]){
+                    isCorrect = true;
+                    counter[0]++;
+                }else{
+                    isCorrect = false;
+                }
+            }
+        }
+    }
+    if(isInside(mouse,{x:595+(310/2)+50,y:689+160,width:310,height:116})){
+        if(isCorrect!=undefined){
+            newQuestion();
+        }
+    }
+    if(toConjugate==true){
+        for(let i=0;i<con.length-1;i++){
+            if(isInside(mouse,{x:cx-320,y:cy-126-(i*modifier),width:310,height:116})){
+                window.location = con[i+1][0];
+            }
+        }        
+    }
+});
+
+function draw(){
+    bg('green')
+    text("Neufrère",cx/2,100,"80px Serif")
+    text(question[0],cx/2,400,"80px Serif")
+    selectColor("lightgray");
+    text(question[1],cx/2,450,"Italic 40px Serif")
+    rectangles()
+    if(isCorrect!=undefined){
+        selectColor("lightgray");
+        if(isInside(mouse,{x:595+(310/2)+50,y:689+160,width:310,height:116})){
+            selectColor("green")
+        }
+        rect(595+(310/2)+50,689+160,310,116);
+        selectColor("black");
+        text("Continue",595+(310/2)+50+(310/2),689+160+75,"60px Serif")
+    }
+    if(toConjugate){
+        for(let i=0;i<con.length-1;i++){
+            selectColor("white");
+            if(isInside(mouse,{x:cx-320,y:cy-126-(i*modifier),width:310,height:116})){selectColor("green")}
+            rect(cx-320,cy-126-(i*modifier),310,116);
+            selectColor("black")
+            text("Study the "+con[i+1][1],cx-320+(310/2),cy-126+38-(i*modifier),"Italic 30px Serif");
+            text("Conjugations",cx-320+(310/2),cy-126+85-(i*modifier),"45px Serif");
+        }
+    }
+    if(debug){
+        drawDebugger();
+    }
+    selectColor("rgba(0,255,0,0.2)");
+    if(counter[1]>0){text(Math.trunc((counter[0]/counter[1])*100)+"%",cx/2,cy-20,"Italic 45px Serif")}
+}
+
+function render(){draw();}
