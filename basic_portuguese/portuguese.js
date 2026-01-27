fb6d3f304d6e8465392903e71131980cb8fbb7a6
Weston <classicmclive@gmail.com>
2026-01-26 21:40:18 -0500

    Add basic Portuguese phrases and translations

---
+++ basic_portuguese/portuguese.js
@@ -0,0 +1,54 @@
+let language = JSON.parse(`
+[
+    [
+        {
+            "g": "Està ___, obrigado.",
+            "e": "That's fine, thanks.",
+            "a": ["frio", false],
+            "b": ["branco", false],
+            "c": ["legal", true],
+            "d": ["sorvete", false]
+        },
+        {
+            "g": "Como ___.",
+            "e": "How are you (informal).",
+            "a": ["vocês", false],
+            "b": ["vá", false],
+            "c": ["vai", true],
+            "d": ["tu", false]
+        },
+        {
+            "g": "___ good.",
+            "e": "I'm good.",
+            "a": ["É", false],
+            "b": ["Sou", false],
+            "c": ["Quero", false],
+            "d": ["Estou", true]
+        },
+        {
+            "g": "___ em conhecê-lo/la.",
+            "e": "Nice to meet you.",
+            "a": ["Encantado", false],
+            "b": ["Viva", false],
+            "c": ["Bom", false],
+            "d": ["Prazer", true]
+        },
+        {
+            "g": "Fala ___?",
+            "e": "Do you speak english? (formal)",
+            "a": ["inglese", false],
+            "b": ["inglie", false],
+            "c": ["englêse", false],
+            "d": ["inglês", true]
+        },
+        {
+            "g": "Estou ___ português.",
+            "e": "I am learning Portuguese.",
+            "a": ["aprenhdo", false],
+            "b": ["a aprender", false],
+            "c": ["aprendo", false],
+            "d": ["aprendendo", true]
+        }
+    ]
+]
+`);
