affc933818d14d4eff8b1236659d02474c0d5014
Weston <classicmclive@gmail.com>
2026-01-26 22:13:56 -0500

    Add How am I lesson to portuguese

--- basic_portuguese/portuguese.js
+++ basic_portuguese/portuguese.js
@@ -48,6 +48,120 @@ let language = JSON.parse(`
             "b": ["a aprender", false],
             "c": ["aprendo", false],
             "d": ["aprendendo", true]
+        },
+        {
+            "g": "Estou falo ___ pouco.",
+            "e": "I speak a little.",
+            "a": ["a", false],
+            "b": ["é", false],
+            "c": ["pão", false],
+            "d": ["um", true]
+        },
+        {
+            "g": "___ repetir?",
+            "e": "Can you repeat. (formal)",
+            "a": ["Podes", false],
+            "b": ["Posso", false],
+            "c": ["Quero", false],
+            "d": ["Pode", true]
+        },
+        {
+            "g": "Eu ___ sei.",
+            "e": "I don't know (formal)",
+            "a": ["noa", false],
+            "b": ["ãou", false],
+            "c": ["non", false],
+            "d": ["não", true]
+        },
+        {
+            "g": "O ___ é isso",
+            "e": "What is this?",
+            "a": ["puoco", false],
+            "b": ["um", false],
+            "c": ["che", false],
+            "d": ["que", true]
+        }
+    ],
+    [
+        {
+            "g": "Eu ___ bem.",
+            "e": "I am good.",
+            "a": ["estou", true],
+            "b": ["sou", false],
+            "c": ["fico", false],
+            "d": ["tenho", false]
+        },
+        {
+            "g": "___ bem.",
+            "e": "All good.",
+            "a": ["Tudo", true],
+            "b": ["Todo", false],
+            "c": ["Todos", false],
+            "d": ["Toda", false]
+        },
+        {
+            "g": "Estou ___.",
+            "e": "I am okay.",
+            "a": ["ok", true],
+            "b": ["bem", false],
+            "c": ["bom", false],
+            "d": ["certo", false]
+        },
+        {
+            "g": "Mais ou ___.",
+            "e": "So-so.",
+            "a": ["menos", true],
+            "b": ["pouco", false],
+            "c": ["nada", false],
+            "d": ["mal", false]
+        },
+        {
+            "g": "Estou ___.",
+            "e": "I am tired.",
+            "a": ["cansado", true],
+            "b": ["fome", false],
+            "c": ["sono", false],
+            "d": ["tranquilo", false]
+        },
+        {
+            "g": "Tô ___.",
+            "e": "I am calm / chill.",
+            "a": ["tranquilo", true],
+            "b": ["rápido", false],
+            "c": ["feliz", false],
+            "d": ["pronto", false]
+        },
+        {
+            "g": "Estou com ___.",
+            "e": "I am hungry.",
+            "a": ["fome", true],
+            "b": ["sono", false],
+            "c": ["frio", false],
+            "d": ["bem", false]
+        },
+        {
+            "g": "Estou com ___.",
+            "e": "I am sleepy.",
+            "a": ["sono", true],
+            "b": ["fome", false],
+            "c": ["cansado", false],
+            "d": ["calma", false]
+        },
+        {
+            "g": "___ mal.",
+            "e": "Not bad.",
+            "a": ["Nada", true],
+            "b": ["Tudo", false],
+            "c": ["Muito", false],
+            "d": ["Bem", false]
+        },
+        {
+            "g": "Está ___.",
+            "e": "It is fine.",
+            "a": ["tudo", false],
+            "b": ["bem", true],
+            "c": ["certo", false],
+            "d": ["ok", false]
         }
     ]
 ]
