39c5b8f8c031f035bd8bda2ab3637450d1368290
Weston <classicmclive@gmail.com>
2026-01-27 23:25:56 -0500

    Fix accidental swap between german and galician conjugations

--- galician/conjugations.js
+++ galician/conjugations.js
@@ -5,73 +5,22 @@ function setConjugation(number){
   pres = cons[number].pres;
 }
 let cons = [
-  // Perfekt is set 0;
+  // Present tense
   {
-    pre:"Wähle hier...", 
+    pre:"Esquive aquí...", 
     pro:[
-      ["I","Ich"],
-      ["You","Du"],
-      ["He / She","Er / Sie"],
-      ["We","Wir"],
-      ["You pl.","Ihr"],
-      ["They / fem.","Sie"],
+      ["I","Eu"],
+      ["You","Ti"],
+      ["He / She","El / Ela"],
+      ["We","Nós"],
+      ["You pl.","Vós"],
+      ["They / fem.","Eles / Elas"],
     ],
     pres:[
-      [["ate", "Essen"], ["habe gegessen", "hast gegessen", "hat gegessen", "haben gegessen", "habt gegessen", "haben gegessen"]],
-      [["went", "Gehen"], ["bin gegangen", "bist gegangen", "ist gegangen", "sind gegangen", "seid gegangen", "sind gegangen"]],
-      [["came", "Kommen"], ["bin gekommen", "bist gekommen", "ist gekommen", "sind gekommen", "seid gekommen", "sind gekommen"]],
-      [["saw", "Sehen"], ["habe gesehen", "hast gesehen", "hat gesehen", "haben gesehen", "habt gesehen", "haben gesehen"]],
-      [["spoke", "Sprechen"], ["habe gesprochen", "hast gesprochen", "hat gesprochen", "haben gesprochen", "habt gesprochen", "haben gesprochen"]],
-      [["found", "Finden"], ["habe gefunden", "hast gefunden", "hat gefunden", "haben gefunden", "habt gefunden", "haben gefunden"]],
-      [["took", "Nehmen"], ["habe genommen", "hast genommen", "hat genommen", "haben genommen", "habt genommen", "haben genommen"]],
-      [["gave", "Geben"], ["habe gegeben", "hast gegeben", "hat gegeben", "haben gegeben", "habt gegeben", "haben gegeben"]],
-      [["slept", "Schlafen"], ["habe geschlafen", "hast geschlafen", "hat geschlafen", "haben geschlafen", "habt geschlafen", "haben geschlafen"]],
-      [["ran", "Laufen"], ["bin gelaufen", "bist gelaufen", "ist gelaufen", "sind gelaufen", "seid gelaufen", "sind gelaufen"]],
-      [["stayed", "Bleiben"], ["bin geblieben", "bist geblieben", "ist geblieben", "sind geblieben", "seid geblieben", "sind geblieben"]],
-      [["wrote", "Schreiben"], ["habe geschrieben", "hast geschrieben", "hat geschrieben", "haben geschrieben", "habt geschrieben", "haben geschrieben"]],
-      [["drank", "Trinken"], ["habe getrunken", "hast getrunken", "hat getrunken", "haben getrunken", "habt getrunken", "haben getrunken"]],
-      [["drove", "Fahren"], ["bin gefahren", "bist gefahren", "ist gefahren", "sind gefahren", "seid gefahren", "sind gefahren"]],
-      [["helped", "Helfen"], ["habe geholfen", "hast geholfen", "hat geholfen", "haben geholfen", "habt geholfen", "haben geholfen"]],
-      [["fell", "Fallen"], ["bin gefallen", "bist gefallen", "ist gefallen", "sind gefallen", "seid gefallen", "sind gefallen"]],
-      [["understood", "Verstehen"], ["habe verstanden", "hast verstanden", "hat verstanden", "haben verstanden", "habt verstanden", "haben verstanden"]],
-      [["swam", "Schwimmen"], ["bin geschwommen", "bist geschwommen", "ist geschwommen", "sind geschwommen", "seid geschwommen", "sind geschwommen"]],
-      [["called", "Rufen"], ["habe gerufen", "hast gerufen", "hat gerufen", "haben gerufen", "habt gerufen", "haben gerufen"]],
-      [["helped", "Helfen"], ["habe geholfen", "hast geholfen", "hat geholfen", "haben geholfen", "habt geholfen", "haben geholfen"]]
-    ]
-  },
-  // Prateritum is set 1;
-  {
-    pre:"Wähle hier...", 
-    pro:[
-      ["I","Ich"],
-      ["You","Du"],
-      ["He / She","Er / Sie"],
-      ["We","Wir"],
-      ["You pl.","Ihr"],
-      ["They / fem.","Sie"],
-    ],
-    pres:[
-      [["was / were", "Sein"], ["war", "warst", "war", "waren", "wart", "waren"]],
-      [["had", "Haben"], ["hatte", "hattest", "hatte", "hatten", "hattet", "hatten"]],
-      [["went", "Gehen"], ["ging", "gingst", "ging", "gingen", "gingt", "gingen"]],
-      [["came", "Kommen"], ["kam", "kamst", "kam", "kamen", "kamt", "kamen"]],
-      [["saw", "Sehen"], ["sah", "sahst", "sah", "sahen", "saht", "sahen"]],
-      [["knew", "Wissen"], ["wusste", "wusstest", "wusste", "wussten", "wusstet", "wussten"]],
-      [["gave", "Geben"], ["gab", "gabst", "gab", "gaben", "gabt", "gaben"]],
-      [["took", "Nehmen"], ["nahm", "nahmst", "nahm", "nahmen", "nahmt", "nahmen"]],
-      [["ate", "Essen"], ["aß", "aßest", "aß", "aßen", "aßt", "aßen"]],
-      [["drank", "Trinken"], ["trank", "trankst", "trank", "tranken", "trankt", "tranken"]],
-      [["found", "Finden"], ["fand", "fandst", "fand", "fanden", "fandet", "fanden"]],
-      [["thought", "Denken"], ["dachte", "dachtest", "dachte", "dachten", "dachtet", "dachten"]],
-      [["wrote", "Schreiben"], ["schrieb", "schriebst", "schrieb", "schrieben", "schriebt", "schrieben"]],
-      [["read (past)", "Lesen"], ["las", "last", "las", "lasen", "last", "lasen"]],
-      [["slept", "Schlafen"], ["schlief", "schliefst", "schlief", "schliefen", "schlieft", "schliefen"]],
-      [["spoke", "Sprechen"], ["sprach", "sprachst", "sprach", "sprachen", "spracht", "sprachen"]],
-      [["ran", "Laufen"], ["lief", "liefst", "lief", "liefen", "lieft", "liefen"]],
-      [["helped", "Helfen"], ["half", "halfst", "half", "halfen", "halft", "halfen"]],
-      [["began", "Beginnen"], ["begann", "begannst", "begann", "begannen", "begannt", "begannen"]],
-      [["remained", "Bleiben"], ["blieb", "bliebst", "blieb", "blieben", "bliebt", "blieben"]],
+      [["to speak", "Falar"],["falo","falas","fala","falamos","falades","falan"]],   
+      [["to eat", "Comer"],["como","comes","come","comemos","comedes","comen"]],   
+      [["to live", "Vivir"],["vivo","vives","vive","vivimos","vivides","viven"]],
+      [["to be","Estar"],["estou","estás","está","estamos","estades","están"]]
     ]
   }
-]
-
+];
